ab6ef38c47ec471e183e718b37c2c3dd4be0c9a2
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-10-31 15:04:30 +0100

    Added lightning talk configuration, deleted duplicated slides

--- slides/how_does_this_work.rb
+++ slides/how_does_this_work.rb
@@ -1,7 +1,7 @@
 class HowDoesThisWork < Wingtips::Slide
   def content
     headline 'How does this work?'
-    bullet 'see the code of this Wingtips::Slide: '
+    bullet 'see the code of this slide: '
     empty_line
     code __FILE__
   end
